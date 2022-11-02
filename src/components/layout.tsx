cae8eb6898085789040bcc9c6ab2b373d36efc7f
kubaszpak <jakub.szpak.it@gmail.com>
2022-11-02 23:20:51 +0100

    Added main title

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,10 +3,10 @@ import Navbar from "./navbar";
 
 export default function Layout(props: { children: React.ReactNode }) {
 	return (
-		<>
+		<div className="flex flex-col">
 			<Navbar />
 			<div className="w-full h-1 bg-[#D3D3D3]" />
 			{props.children}
-		</>
+		</div>
 	);
 }
