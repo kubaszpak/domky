dae4acb26a76d5f4050f119fc2fa9f342486df3e
kubaszpak <jakub.szpak.it@gmail.com>
2022-12-18 19:42:06 +0100

    Cleaning and added error pages

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+const Error = () => {
+	return (
+		<div className="flex-1 h-full=w-full flex justify-center items-center">
+			<h1 className="font-semibold text-3xl text-gray-600">
+				404 - Page not found
+			</h1>
+		</div>
+	);
+};
+
+export default Error;
---
+++ src/pages/500.tsx
@@ -0,0 +1,11 @@
+const Error = () => {
+	return (
+		<div className="flex-1 h-full=w-full flex justify-center items-center">
+			<h1 className="font-semibold text-3xl text-gray-600">
+				500 - Internal Server Error
+			</h1>
+		</div>
+	);
+};
+
+export default Error;
