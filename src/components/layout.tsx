efe6148d3616d88baaa4b9869b9bf1084d67eb8b
kubaszpak <jakub.szpak.it@gmail.com>
2023-01-07 17:10:17 +0100

    Moved Head section to layout for it to appear on every subpage and added spinner to main page

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,23 @@
+import Head from "next/head";
 import React from "react";
 import Navbar from "./navbar";
 
 export default function Layout(props: { children: React.ReactNode }) {
 	return (
-		<div className="flex flex-col">
-			<Navbar />
-			<div className="w-full h-1 bg-[#D3D3D3]" />
-			{props.children}
-		</div>
+		<>
+			<Head>
+				<title>domky</title>
+				<meta
+					name="description"
+					content="Website to provide free accommodation offers for people in need in Poland"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<div className="flex flex-col">
+				<Navbar />
+				<div className="w-full h-1 bg-[#D3D3D3]" />
+				{props.children}
+			</div>
+		</>
 	);
 }
