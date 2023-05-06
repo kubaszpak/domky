02d3d6c8f197800104abb9c74dff1bf9c38ad72d
kubaszpak <jakub.szpak.it@gmail.com>
2023-05-07 00:44:27 +0200

    Made description and message fields longer

--- src/utils/schemas.ts
+++ src/utils/schemas.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 export const createSchema = z.object({
 	name: z.string(),
 	guests: z.number().min(1).max(10),
-	description: z.string().max(191),
+	description: z.string(),
 	longitude: z.number(),
 	latitude: z.number(),
 	date_start: z.date(),
